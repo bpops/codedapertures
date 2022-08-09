950477c033df06b7637ec7e5fceb52f81078b652
Brent <bpops@users.noreply.github.com>
2022-08-08 21:00:53 -0500

    cappy package now installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='codedapertures',
+      version='0.1',
+      description='Coded Aperture Protudction in PYthon',
+      url='https://github.com/bpops/cappy',
+      author='bpops',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
