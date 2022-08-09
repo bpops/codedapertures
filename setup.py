06110b299f42516be49c81f0c14bb48521a48d60
Brent <bpops@users.noreply.github.com>
2022-08-08 21:58:22 -0500

    finalized upload to pypi, cappy now installable with pip

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 setuptools.setup(name='codedapertures',
       version='0.1',
-      description='Coded Aperture Protudction in PYthon',
+      description='Coded Aperture Production in PYthon',
+      long_description='Coded Aperture Production in PYthon',
       url='https://github.com/bpops/cappy',
       author='bpops',
       license='MIT',
