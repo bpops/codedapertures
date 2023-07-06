a049b1b5ba4fac11d9ce52b684ee70563b35c170
bpops <bpops@users.noreply.github.com>
2023-07-05 19:44:51 -0500

    updated setup.py for new version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 setuptools.setup(name='codedapertures',
-      version='0.1',
-      description='Coded Aperture Production in PYthon',
-      long_description='Coded Aperture Production in PYthon',
+      version='0.1.1',
+      description='Coded Aperture Production in PYthon (CAPPY)',
+      long_description='Coded Aperture Production in PYthon (CAPPY)',
       url='https://github.com/bpops/cappy',
       author='bpops',
       license='MIT',
